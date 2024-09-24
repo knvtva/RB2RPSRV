62efa5291e3f8b256d1097b5b05caa81c0f8a2f9
li1lypad <143225056+li1lypad@users.noreply.github.com>
2024-09-24 19:23:54 +0100

    Make config accessible in other places.

--- src/config/config.go
+++ src/config/config.go
@@ -6,17 +6,25 @@ import (
 	"gopkg.in/ini.v1"
 )
 
+var Config struct {
+	AccessKey    string
+	AuthPort     string
+	SecurePort   string
+	UserPassword string
+	MongoDBURI   string
+}
+
 func ReadConfig() {
 	cfg, err := ini.Load("config.ini")
 	if err != nil {
 		log.Fatalf("Fail to read file: %v", err)
 		os.Exit(1)
 	}
-	
-	AccessKey := cfg.Section("Server").Key("AccessKey").String()
-	AuthPort := cfg.Section("Server").Key("AuthPort")
-	SecurePort := cfg.Section("Server").Key("SecurePort")
-	UserPassword := cfg.Section("Server").Key("UserPassword").String()
 
-	MongoDBURI := cfg.Section("Database").Key("URI").String()
+	Config.AccessKey = cfg.Section("Server").Key("AccessKey").String()
+	Config.AuthPort = cfg.Section("Server").Key("AuthPort").String()
+	Config.SecurePort = cfg.Section("Server").Key("SecurePort").String()
+	Config.UserPassword = cfg.Section("Server").Key("UserPassword").String()
+	
+	Config.MongoDBURI = cfg.Section("Database").Key("URI").String()
 }
