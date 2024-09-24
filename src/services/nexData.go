c81e9cc72aea0f1a016c49cc7e55e28475cc862d
Ash <87194661+knvtva@users.noreply.github.com>
2024-09-24 13:34:11 +0100

    Implement a basic auth
    This needs to be developed a lot further. This is just a basic template as of now.
    
    TODO:
    Decrypt payload
    GetTicketData
    Database

---
+++ src/services/nexData.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"github.com/knvtva/nex-go"
+)
+
+var Auth *nex.Server
+
+func SetupAuth() {
+	Auth = nex.NewServer()
+	Auth.SetPrudpVersion(0)
+	Auth.SetSignatureVersion(1)
+    	Auth.SetKerberosKeySize(16)
+    	Auth.SetAccessKey("FindWiiAccessKey")
+}
