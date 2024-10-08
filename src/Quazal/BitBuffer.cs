aa6d9b6e8a9469b25dfda6f22a3cd416e106d169
Ash <87194661+knvtva@users.noreply.github.com>
2024-10-08 14:17:18 +0100

    Implement basic Quazal BitBuffer

---
+++ src/Quazal/BitBuffer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quazal
+{
+    public class BitBuffer
+    {
+        MemoryStream buffer;
+        public int byteSize;
+        public int bitSize;
+        public int bitPos;
+
+        public BitBuffer()
+        {
+            buffer = new MemoryStream();
+            byteSize = 0;
+            bitSize = 0;
+            bitPos = 0;
+            buffer.WriteByte(0);
+        }
+    }
+}
