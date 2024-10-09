936b76a37b82231f9d2e49ec826e184757d43074
li1lypad <143225056+li1lypad@users.noreply.github.com>
2024-10-09 15:36:44 +0100

    I'm dumb

--- src/Quazal/QPacketHandler.cs
+++ src/Quazal/QPacketHandler.cs
@@ -102,63 +102,64 @@ namespace Quazal
 
           public static void ProcessPacket(string source, byte[] data, IPEndPoint ep, UdpClient listener, uint serverPID, ushort listenPort, bool removeConnectPayload = false)
           {
-            QPacket p = new QPacket(data);
-            MemoryStream m = new MemoryStream();
-            byte[] buff = new byte[(int)p.realSize];
-
-            m.Seek(0, 0);
-            m.Read(buff, 0, buff.Length);
-
-            QPacket reply = null;
-            Client client = null;
-
-            if(p.type != QPacket.PACKETTYPE.SYN && p.type != QPacket.PACKETTYPE.NATPING)
+            while (true)
             {
-              client = Server.GetClientByIDrecv(p.m_uiSignature);
-            }
-            switch (p.type)
-            {
-              case QPacket.PACKETTYPE.SYN:
-                  reply = QPacketHandler.ProcessSYN(p, ep, out client);
+              QPacket p = new QPacket(data);
+              MemoryStream m = new MemoryStream();
+              byte[] buff = new byte[(int)p.realSize];
+
+              m.Seek(0, 0);
+              m.Read(buff, 0, buff.Length);
+
+              QPacket reply = null;
+              Client client = null;
+
+              if(p.type != QPacket.PACKETTYPE.SYN && p.type != QPacket.PACKETTYPE.NATPING)
+              {
+                client = Server.GetClientByIDrecv(p.m_uiSignature);
+              }
+              switch (p.type)
+              {
+                case QPacket.PACKETTYPE.SYN:
+                    reply = QPacketHandler.ProcessSYN(p, ep, out client);
+                    break;
+                case QPacket.PACKETTYPE.CONNECT:
+                    if (client != null && !p.flags.Contains(QPacket.PACKETFLAG.FLAG_ACK))
+                          {
+                              client.sPID = serverPID;
+                              client.sPort = listenPort;
+                              if (removeConnectPayload)
+                              {
+                                  p.payload = new byte[0];
+                                  p.payloadSize = 0;
+                              }
+                              reply = QPacketHandler.ProcessCONNECT(client, p);
+                          }
+                    break;
+                case QPacket.PACKETTYPE.DISCONNECT:
+                    if (client != null)
+                        reply = QPacketHandler.ProcessDISCONNECT(client, p);
+                        break;
+                case QPacket.PACKETTYPE.PING:
+                    if (client != null)
+                        reply = QPacketHandler.ProcessPING(client, p);
+                        break;
+              }
+
+              if (reply != null)
+                Send(source, reply, ep, listener);
+              
+              if (p.realSize != data.Length)
+              {
+                m = new MemoryStream(data);
+                int left = (int)(data.Length - p.realSize);
+                byte[] newData = new byte[left];
+                m.Seek(p.realSize, 0);
+                m.Read(newData, 0, left);
+                data = newData;
+              }
+              else
                   break;
-              case QPacket.PACKETTYPE.CONNECT:
-                  if (client != null && !p.flags.Contains(QPacket.PACKETFLAG.FLAG_ACK))
-                        {
-                            client.sPID = serverPID;
-                            client.sPort = listenPort;
-                            if (removeConnectPayload)
-                            {
-                                p.payload = new byte[0];
-                                p.payloadSize = 0;
-                            }
-                            reply = QPacketHandler.ProcessCONNECT(client, p);
-                        }
-                  break;
-              case QPacket.PACKETTYPE.DISCONNECT:
-                  if (client != null)
-                      reply = QPacketHandler.ProcessDISCONNECT(client, p);
-                      break;
-              case QPacket.PACKETTYPE.PING:
-                  if (client != null)
-                      reply = QPacketHandler.ProcessPING(client, p);
-                      break;
-            }
-
-            if (reply! = null)
-              Send(source, reply, ep, listener);
-            
-            if (p.realSize != data.Length)
-            {
-              m = new MemoryStream(data);
-              int left = (int)(data.Length - p.realSize);
-              byte[] newData = new byte[left];
-              m.Seek(p.realSize, 0);
-              m.Read(newData, 0, left);
-              data = newData;
-            }
-            else
-            {
-              break;
             }
           }
 
