4a123c193943ae5350912508085afd9b2c953223
li1lypad <143225056+li1lypad@users.noreply.github.com>
2024-10-09 13:00:25 +0100

    Implement QPacketHandler, Basic ProcessPacket implementation

---
+++ src/Quazal/QPacketHandler.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+namespace Quazal
+{
+    public static class QPacketHandler
+    {
+          public static void ProcessPacket(string source, byte[] data, IPEndPoint ep, UdpClient listener, uint serverPID, ushort listenPort, bool removeConnectPayload = false)
+          {
+           continue;
+          }
+    }
+}
