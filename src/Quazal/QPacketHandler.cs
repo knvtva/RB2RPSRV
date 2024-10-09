5b4963298cccc4a8abe9051d63dd7e44bb87554e
li1lypad <143225056+li1lypad@users.noreply.github.com>
2024-10-09 13:05:37 +0100

    lily it's not a loop.. WHY DID YOU ADD CONTINUE?

--- src/Quazal/QPacketHandler.cs
+++ src/Quazal/QPacketHandler.cs
@@ -7,6 +7,6 @@ namespace Quazal
     public static class QPacketHandler
     {
           public static void ProcessPacket(string source, byte[] data, IPEndPoint ep, UdpClient listener, uint serverPID, ushort listenPort, bool removeConnectPayload = false)
-          { continue; }
+          { // }
     }
 }
