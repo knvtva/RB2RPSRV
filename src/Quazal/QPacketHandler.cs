bc6171df60c364e09f4ce76fd788cdb85a4c4248
li1lypad <143225056+li1lypad@users.noreply.github.com>
2024-10-09 14:26:40 +0100

    Finish Implementing Process Functions

--- src/Quazal/QPacketHandler.cs
+++ src/Quazal/QPacketHandler.cs
@@ -31,6 +31,72 @@ namespace Quazal
             return reply;
           }
 
+          public static QPacket ProcessCONNECT(Client client, QPacket p)
+          {
+            client.IDsend = p.m_uiConnectionSignature;
+            QPacket reply = new QPacket();
+            reply.m_oSourceVPort = p.m_oDestinationVPort;
+            reply.m_oDestinationVPort = p.m_oSourceVPort;
+            reply.flags = new List<QPacket.PACKETFLAG>() { QPacket.PACKETFLAG.FLAG_ACK };
+            reply.type = QPacket.PACKETTYPE.CONNECT;
+            reply.m_bySessionID = p.m_bySessionID;
+            reply.m_uiSignature = p.m_uiSignature;
+            reply.uiSeqId = p.uiSeqId;
+            reply.m_uiConnectionSignature = client.IDsend;
+            if (p.payload != null && p.payload.Length > 0)
+              reply.payload = MakeConnectPayload(client,p);
+            else
+              reply.payload = new byte[0];
+            return reply; 
+          }
+
+          public static byte[] makeConnectPayload(Client client, QPacket p)
+          {
+            MemoryStream m = new MemoryStream(p.payload);
+            uint size = DataWriter.ReadUInt32(m);
+            byte[] buff = new byte[size];
+            m.Read(buff, 0, (int)size);
+            buff = new byte[size];
+            m.Read(buff, 0, (int)size);
+            buff = DataWriter.Decrypt(client.sessionKey, buff);
+            m = new MemoryStream(buff);
+            DataWriter.ReadUint32(m);
+            DataWriter.ReadUint32(m);
+            uint responseCode = DataWriter.ReadUint32(m);
+            m = new MemoryStream();
+            DataWriter.WriteUint32(m, 4);
+            DataWriter.WriteUint32(m, responseCode + 1);
+            return m.ToArray();
+          }
+
+          public static QPacket ProcessDISCONNECT(Client client, QPacket p)
+          {
+            QPacket reply = new QPacket();
+            reply.m_oSourceVPort = p.m_oDestinationVPort;
+            reply.m_oDestinationVPort = p.m_oSourceVPort;
+            reply.flags = new List<QPacket.PACKETFLAG>() { QPacket.PACKETFLAG.FLAG_ACK };
+            reply.type = QPacket.PACKETTYPE.DISCONNECT;
+            reply.m_bySessionID = p.m_bySessionID;
+            reply.m_uiSignature = client.IDsend - 0x10000;
+            reply.uiSeqId = p.uiSeqId;
+            reply.payload = new byte[0];
+            return reply;
+          }
+
+          public static QPacket ProcessPING(ClientInfo client, QPacket p)
+          {
+            QPacket reply = new QPacket();
+            reply.m_oSourceVPort = p.m_oDestinationVPort;
+            reply.m_oDestinationVPort = p.m_oSourceVPort;
+            reply.flags = new List<QPacket.PACKETFLAG>() { QPacket.PACKETFLAG.FLAG_ACK };
+            reply.type = QPacket.PACKETTYPE.PING;
+            reply.m_bySessionID = p.m_bySessionID;
+            reply.m_uiSignature = client.IDsend;
+            reply.uiSeqId = p.uiSeqId;
+            reply.m_uiConnectionSignature = client.IDrecv;
+            reply.payload = new byte[0];
+            return reply;
+          }
 
           public static void ProcessPacket(string source, byte[] data, IPEndPoint ep, UdpClient listener, uint serverPID, ushort listenPort, bool removeConnectPayload = false)
           {
