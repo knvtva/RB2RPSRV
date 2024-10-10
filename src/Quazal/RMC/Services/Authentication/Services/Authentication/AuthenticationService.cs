41b46f20711f4b614373770bfc8d3302c3359cdb
li1lypad <143225056+li1lypad@users.noreply.github.com>
2024-10-10 09:48:39 +0100

    Ticket Typo

---
+++ src/Quazal/RMC/Services/Authentication/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Quazal
+{
+    public static class AuthenticationService
+    {
+        public static void ProcessAuthenticationServiceRequest(Stream s, RMCP rmc)
+        {
+            switch(rmc.methodID)
+            {
+                case 1:
+                    rmc.request = new RMCPacketRequestLogin(s);
+                    break;
+                case 3:
+                    rmc.request = new RMCPacketRequestTicket(s);
+                    break;
+                default:
+                    Logger.Error("[RMC Authentication] Unknown Method ID" + rmc.methodID);
+                    break;
+            }
+        }
+
+        public static void HandleAuthenticationServiceRequest(QPacket p, RMCP rmc, Client client)
+        {
+            RMCPResponse reply;
+            switch (rmc.methodID)
+            {
+                case 1:
+                    Logger.Error("[RMC Authentication] Login Request Received but not implemented");
+                case 3:
+                    reply = new RMCPacketResponseRequestTicket(client.PID, client.sPID);
+                    RMC.SendResponseWithACK(client.udp, p, rmc, client, reply);
+                    break;
+                default:
+                    Logger.Error("[RMC Authentication] Unknown Method ID" + rmc.methodID);
+            }
+        }
+    }
+}
