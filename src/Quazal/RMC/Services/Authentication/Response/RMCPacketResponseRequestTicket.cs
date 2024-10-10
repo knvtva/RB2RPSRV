5a8432337df03bfedd9aea142900a3234cd8f43d
li1lypad <143225056+li1lypad@users.noreply.github.com>
2024-10-10 09:55:00 +0100

    Remove RMCPResponse

--- src/Quazal/RMC/Services/Authentication/Response/RMCPacketResponseRequestTicket.cs
+++ src/Quazal/RMC/Services/Authentication/Response/RMCPacketResponseRequestTicket.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Quazal
 {
-    public class RMCPacketResponseRequestTicket : RMCPResponse
+    public class RMCPacketResponseRequestTicket
     {
         public uint resultCode = 0x10001;
         public byte[] ticketBuffer;
